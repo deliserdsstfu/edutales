1069124bc87271334095f3735942a820b8380082
Patrick Sacher <patzi@Patricks-MacBook-Pro.local>
2020-01-05 11:59:06 +0100

    deleted

--- backend/venv/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\stefa\Documents\projects\angular-rest\backend\venv\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
