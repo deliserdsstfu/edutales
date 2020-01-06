bcccb58de777dceb83f14ff8925e588b23a946ed
Clemen5 <45812824+Clemen5@users.noreply.github.com>
2020-01-06 14:18:26 +0100

    Frontend
    Bootstrap implementation

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
+
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
+import {HttpClientModule} from '@angular/common/http';
+import { ParentFormComponent } from './parent-form/parent-form.component';
+import {AppRoutingModule} from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ParentFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    NgbModule.forRoot(),
+    FormsModule,
+    RouterModule,
+    ComponentsModule,
+    ExamplesModule,
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
