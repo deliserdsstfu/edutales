61e124855748155ebd4a1eebab29399b6f564761
Clemen5 <45812824+Clemen5@users.noreply.github.com>
2020-01-10 14:46:46 +0100

    Child Form Component

--- frontend/src/app/child-form/child-form.component.ts
+++ frontend/src/app/child-form/child-form.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ChildService} from '../service/child.service';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AbstractControl, AsyncValidatorFn, FormBuilder, ValidationErrors, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Component({
     selector: 'app-child-form',
@@ -7,9 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildFormComponent implements OnInit {
 
-  constructor() { }
+    childFormGroup;
+
+
+    constructor(private fb: FormBuilder, private childService: ChildService, private route: ActivatedRoute,
+                private router: Router) {
+    }
 
   ngOnInit() {
+
+      const data = this.route.snapshot.data;
+
+      this.childFormGroup = this.fb.group({
+          id: [null],
+          user_name: ['', [Validators.required]],
+          day_of_birth: [90, [Validators.max(18)]],
+      });
+
+      if (data.child) {
+          this.childFormGroup.patchValue(data.child);
+      }
   }
+    // tslint:disable-next-line:one-line
+    createChild(){
+        const child = this.childFormGroup.value;
+        if (child.id) {
+            this.childService.updateChild(child)
+                .subscribe(() => {
+                    alert('updated successfully');
+                });
+        } else {
+            this.childService.createChild(child)
+                .subscribe((response: any) => {
+                    this.router.navigate(['/child-form/' + response.id]);
+                });
+        }
+    }
+    user_nameValidator(): AsyncValidatorFn {
+        return (control: AbstractControl): Promise<ValidationErrors | null> | Observable<ValidationErrors | null> => {
+            return this.childService.getBooks()
+                .pipe(
+                    map((childs: any[]) => {
+                        const currentId = this.childFormGroup.controls.id.value;
+                        const currentUser_Name = this.childFormGroup.controls.user_name.value;
+                        const childWithSameUser_Name = childs.find((m) => {
+                            return (currentId || m.id !== currentId) && m.title === currentUser_Name;
+                        });
+                        if (childWithSameUser_Name) {
+                            return {
+                                titleAlreadyExists: true
+                            };
+                        } else {
+                            return null;
+                        }
+                    })
+                );
+        };
+    }
 
 }
+
+
+
--- frontend/src/app/service/child.service.ts
+++ frontend/src/app/service/child.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
@@ -6,5 +7,28 @@ import { Injectable } from '@angular/core';
 export class ChildService {
 // Child Services
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  getBooks() {
+    return this.http.get('/api/child/list');
+  }
+
+  createChild(child) {
+    return this.http.post('/api/child/create', child);
+  }
+
+  updateChild(child) {
+    return this.http.put('/api/child/' + child.id + '/update', child);
+  }
+
+  getChild(id) {
+    return this.http.get('/api/child/' + id + '/get');
+  }
+
+  deleteChild(child) {
+    return this.http.delete('/api/child/' + child.id + '/delete');
+  }
 }
+
+
