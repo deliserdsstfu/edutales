171d7418060cd8d5e689d0d8be22145b40c6c5f7
xbrunnemi <michi.brunner@gmail.com>
2020-01-10 10:54:50 +0100

    Child

--- frontend/src/app/child-form/child-form.component.ts
+++ frontend/src/app/child-form/child-form.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from '@angular/forms';
+import {HttpClient} from '@angular/common/http';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ChildService} from '../service/child.service';
 
 @Component({
-  selector: 'app-child-form',
-  templateUrl: './child-form.component.html',
-  styleUrls: ['./child-form.component.scss']
+    selector: 'app-child-form',
+    templateUrl: './child-form.component.html',
+    styleUrls: ['./child-form.component.scss']
 })
 export class ChildFormComponent implements OnInit {
+    focus;
+    childFormGroup;
 
-  constructor() { }
+    constructor(private fb: FormBuilder, private http: HttpClient, private route: ActivatedRoute,
+                private router: Router, private childService: ChildService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.childFormGroup = this.fb.group({
+            'cid': [null],
+            'user_name': ['', Validators.required],
+            'year_of_birth': [1, Validators.max(2000)],
+        });
 
+        const cid = this.route.snapshot.paramMap.get('cid');
+        if (cid) {
+            this.http.get('/api/child/' + cid + '/get')
+                .subscribe((response) => {
+                    this.childFormGroup.patchValue(response, {emitEvent: false});
+                });
+        }
+    }
+
+    createChild() {
+        const child = this.childFormGroup.value;
+        this.childService.createChild(child)
+            .subscribe(() => {
+                this.router.navigate(['/login/']);
+            });
+    }
 }
--- frontend/src/app/service/child.service.ts
+++ frontend/src/app/service/child.service.ts
@@ -1,9 +1,31 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class ChildService {
 
-  constructor() { }
+    constructor(private http: HttpClient) {
+    }
+
+    getChildren() {
+        return this.http.get('/api/child/list');
+    }
+
+    getChild(cid) {
+        return this.http.get('/api/child/' + cid + '/get');
+    }
+
+    createChild(child) {
+        return this.http.post('/api/child/create', child);
+    }
+
+    updateChild(child) {
+        return this.http.put('/api/child/' + child.cid + '/update', child);
+    }
+
+    deleteChild(child) {
+        return this.http.delete('/api/child/' + child.cid + '/delete', child);
+    }
 }
